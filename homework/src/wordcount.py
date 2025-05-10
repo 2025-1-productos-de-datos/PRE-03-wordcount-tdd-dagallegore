8a2593e2ac8c8f91261ae4167a07eeecab3de430
dagallegore <dagallegore@unal.edu.co>
2025-05-10 09:41:54 -0500

    Guardado 9:41

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,7 @@
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
 
+
+def main():
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
